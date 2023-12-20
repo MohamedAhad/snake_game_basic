8efa87e4080ad559a1c77ee0fd50242db47bad67
mdahad <mohamedahad17@gmail.com>
2023-12-21 00:14:12 +0530

    Added maintaining high score feature

--- scoreboard.py
+++ scoreboard.py
@@ -7,6 +7,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -14,13 +16,18 @@ class Scoreboard(Turtle):
         self.update_score()
 
     def update_score(self):
-        self.write(f"Score: {self.score}", align=ALLIGNMENT, font=FONT)
-
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"Game Over", align=ALLIGNMENT, font=FONT)
-    def increase_score(self):
         self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALLIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.high_score}")
+        self.score = 0
+        self.update_score()
+
+    def increase_score(self):
         self.score += 1
         self.update_score()
 
